b9f9edbf121579ffdbaf27ad4b0a43bf123d98aa
daffapandora <163204909+daffapandora@users.noreply.github.com>
2024-03-13 16:52:12 +0700

    Update CekBilSemp.cpp

--- Lab sesi 3/CekBilSemp.cpp
+++ Lab sesi 3/CekBilSemp.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int N, sum = 0, i, count = 0;
+    int N, sum = 0, i;
 
     printf("Masukan N : ");
     scanf("%d", &N);
 
-    for(i = 1; i <= N; i++)
+    for(i = 1; i <= N/2; i++)
     {
         if(N % i == 0)
         {
             sum += i;
-            count++;
         }
     }
 
