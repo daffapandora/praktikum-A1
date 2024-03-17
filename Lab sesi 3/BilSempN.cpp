9a01f3e4ca3838bd4b9b4797148d815917dfa3c1
daffapandora <163204909+daffapandora@users.noreply.github.com>
2024-03-17 21:21:12 +0700

    Add files via upload

--- Lab sesi 3/BilSempN.cpp
+++ Lab sesi 3/BilSempN.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int BilSempN(int num) {
+int isPerfect(int num) {
     int sum = 0;
     for (int i = 1; i <= num / 2; i++) {
         if (num % i == 0) {
@@ -10,19 +10,19 @@ int BilSempN(int num) {
     return (sum == num);
 }
 
-void x(int N) {
-    printf("Bilangan sempurna hingga %d: ", N);
+int main() {
+    int N;
+
+    printf("Masukkan nilai N : ");
+    scanf("%d", &N);
+
+    printf("Bilangan sempurna hingga %d : ", N);
     for (int i = 2; i <= N; i++) {
-        if (BilSempN(i)) {
+        if (isPerfect(i)) {
             printf("%d, ", i);
         }
     }
-}
 
-int main() {
-    int N;
-    printf("Masukkan nilai N: ");
-    scanf("%d", &N);
-    x(N);
     return 0;
 }
+
